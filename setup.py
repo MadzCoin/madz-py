4f87bc4ab0fac1431d9cd5ec67b6bed8372e1ae6
Gustas Savickas <75716744+the-red-eye-studio@users.noreply.github.com>
2022-07-24 12:42:09 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,18 +5,18 @@ readme = Path(__file__).parent
 readme = (readme / "README.md").read_text()
 
 setup(
-    name = "siricoin",
-    packages = ["siricoin"],
+    name = "sirious",
+    packages = ["sirious"],
     version = "0.5",
     license= "MIT",
-    description = "SiriCoin RPC Client",
+    description = "Sirious RPC Client",
     long_description = readme,
     long_description_content_type = "text/markdown",
     author = "Bastel Pichi",
     author_email = "pichi@pichisdns.com",
-    url = "https://github.com/BastelPichi/siricoin-py",
+    url = "https://github.com/Sirious-io/sirious-py",
     download_url = "https://github.com/BastelPichi/siricoin-py/archive/refs/tags/0.5.tar.gz",
-    keywords = ["SiriCoin", "Crypto", "RPC"],
+    keywords = ["Sirious", "Crypto", "RPC"],
     install_requires = [
         "web3",
         "eth-account",
