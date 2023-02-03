19513bfea0381935556cee42c80fa892276c648e
Gustas Savickas <75716744+the-red-eye-studio@users.noreply.github.com>
2023-02-03 18:32:41 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,18 +5,18 @@ readme = Path(__file__).parent
 readme = (readme / "README.md").read_text()
 
 setup(
-    name = "sirious",
-    packages = ["sirious"],
+    name = "madz-py",
+    packages = ["madz-py"],
     version = "0.5",
     license= "MIT",
-    description = "Sirious RPC Client",
+    description = "Madzcoin RPC Client",
     long_description = readme,
     long_description_content_type = "text/markdown",
     author = "Bastel Pichi",
     author_email = "pichi@pichisdns.com",
-    url = "https://github.com/Sirious-io/sirious-py",
-    download_url = "https://github.com/BastelPichi/siricoin-py/archive/refs/tags/0.5.tar.gz",
-    keywords = ["Sirious", "Crypto", "RPC"],
+    url = "https://github.com/MadzCoin/madz-py/",
+    download_url = "https://github.com/MadzCoin/madz-py/archive/refs/heads/main.zip",
+    keywords = ["Madzcoin", "Crypto", "RPC"],
     install_requires = [
         "web3",
         "eth-account",
